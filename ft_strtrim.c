57ff78dcfa3727c31b2c8fe778a8021351ad2496
Henri Patsi <hpatsi@c1r1p8.hive.fi>
2023-11-07 09:01:21 +0200

    added a fix to ft_strtrim for empty string + set

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hpatsi <hpatsi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 14:41:37 by hpatsi            #+#    #+#             */
-/*   Updated: 2023/10/31 10:51:56 by hpatsi           ###   ########.fr       */
+/*   Updated: 2023/11/07 08:59:21 by hpatsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,18 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	start;
 	size_t	end;
 	char	*trimmed;
+	size_t	strlen;
 
 	if (s1 == 0)
 		return (0);
 	start = 0;
 	while (in_set(s1[start], set) && s1[start] != 0)
 		start++;
-	end = ft_strlen(s1) - 1;
+	strlen = ft_strlen(s1);
+	if (strlen != 0)
+		end = strlen - 1;
+	else
+		end = 0;
 	while (in_set(s1[end], set) && end > start)
 		end--;
 	trimmed = ft_substr(s1, start, end - start + 1);
