4356ddde760ef22009ccb7c62165663319084251
Henri Patsi <hpatsi@c1r1p7.hive.fi>
2023-11-21 15:44:21 +0200

    removed itoh and ltoh because they were not needed anymore

--- libft.h
+++ libft.h
@@ -51,8 +51,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
-char	*ft_itoh(unsigned int n, int use_captials);
-char	*ft_ltoh(unsigned long n, int use_capitals);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
