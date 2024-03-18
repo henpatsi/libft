4b2c22904e76923ba98989c22d0f56af93681b5b
Henri Pätsi <patsihenri@gmail.com>
2024-03-18 16:41:19 +0200

    made docs files, removed bad tests, added descriptions to some prototypes for e.g. vscode hover view

--- libft.h
+++ libft.h
@@ -27,20 +27,178 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
-int		ft_isalpha(int c);
+//--- TYPE FUNCTIONS ---//
+
+/**
+ * Checks if the given int is the decimal ascii number of a digit.
+ * 
+ * @param[in] c The int to be checked
+ * @returns Returns 1 if c corresponds to a digit ('0' - '9'), else returns 0.
+ */
 int		ft_isdigit(int c);
+
+/**
+ * Checks if the given int is the decimal ascii number of a letter.
+ * 
+ * @param[in] c The int to be checked
+ * @returns Returns 1 if c corresponds to a letter ('a' - 'z' or 'A' - 'Z'), else returns 0.
+ */
+int		ft_isalpha(int c);
+
+/**
+ * Checks if the given int is the decimal ascii number of a digit or a letter.
+ * 
+ * @param[in] c The int to be checked
+ * @returns Returns 1 if c corresponds to a digit ('0' - '9') or a letter ('a' - 'z' or 'A' - 'Z'), else returns 0.
+ */
 int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
+
+/**
+ * Checks if the given int is the decimal ascii number of a space.
+ * 
+ * @param[in] c The int to be checked
+ * @returns Returns 1 if c corresponds to a space (' ', '\t', '\n', '\v', '\f', '\r'), else returns 0.
+ */
 int		ft_isspace(int c);
 
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-void	*ft_memset(void *b, int c, size_t len);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
+/**
+ * Checks if the given int is in the ascii range.
+ * 
+ * @param[in] c The int to be checked
+ * @returns Returns 1 if c is a decimal ascii number (0 - 127), else returns 0.
+ */
+int		ft_isascii(int c);
+
+/**
+ * Checks if the given int is in the printable ascii range.
+ * 
+ * @param[in] c The int to be checked
+ * @returns Returns 1 if c is a printable decimal ascii number (32 - 126), else returns 0.
+ */
+int		ft_isprint(int c);
+
+//--- MEMORY FUNCTIONS ---//
+
+/**
+ * Searches a given length from a memory area for a byte of specific value.
+ * 
+ * @param[in] s The memory area to be searched
+ * @param[in] c The value of the byte to search for
+ * @param[in] n The number of bytes to search from the start
+ * @returns Returns a pointer to the first occurence of c if found, else returns 0.
+ */
 void	*ft_memchr(const void *s, int c, size_t n);
+
+/**
+ * Compares two memory areas up to a given length.
+ * 
+ * @param[in] s1 The first areas
+ * @param[in] s2 The second areas
+ * @param[in] n The number of bytes to compare
+ * @returns On the first occurence of a differing value, returns their difference. Returns 0 if s1 and s2 are identical.
+ */
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
+/**
+ * Copies the specified amount of bytes from the source to the destination.
+ * 
+ * Undefined if source and destination overlap.
+ * 
+ * @param[in] dst The destination to copy to
+ * @param[in] src The source to copy from
+ * @param[in] n The number of bytes to copy
+ * @returns Returns a pointer to dst.
+ */
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+
+/**
+ * Copies the specified amount of bytes from the source to the destination.
+ * 
+ * Always copies the original content of the source to the destination, even if they overlap.
+ * 
+ * @param[in] dst The destination to copy to
+ * @param[in] src The source to copy from
+ * @param[in] n The number of bytes to copy
+ * @returns Returns a pointer to dst.
+ */
+void	*ft_memmove(void *dst, const void *src, size_t len);
+
+/**
+ * Sets a specified amount of bytes to a given value.
+ * 
+ * @param[in] b The memory area to start from
+ * @param[in] c The value the bytes will be set to
+ * @param[in] len The number of bytes to set to the given value
+ * @returns Returns a pointer to b.
+ */
+void	*ft_memset(void *b, int c, size_t len);
+
+/**
+ * Sets a specified amount of bytes to 0.
+ * 
+ * @param[in] s The memory area to start from
+ * @param[in] n The number of bytes to set to the given value
+ */
+void	ft_bzero(void *s, size_t n);
+
+/**
+ * Allocates memory using malloc, then zeroes the allocated memory.
+ * 
+ * @param[in] count The amount of memory blocks to allocate
+ * @param[in] size The size of each memory block
+ * @returns Returns a pointer to the start of the allocated memory.
+ */
+void	*ft_calloc(size_t count, size_t size);
+
+//--- CONVERSION FUNCTIONS ---//
+
+/**
+ * Converts a lowercase letter into an uppercase letter.
+ * 
+ * @param[in] c The int to be converted
+ * @returns Returns the ascii code corresponding to the uppercase letter 
+ * if c corresponds to the ascii code of a lowercase letter, else returns c.
+ */
+int		ft_toupper(int c);
+
+/**
+ * Converts an uppercase letter into a lowercase letter.
+ * 
+ * @param[in] c The int to be converted
+ * @returns Returns the ascii code corresponding to the lowercase letter 
+ * if c corresponds to the ascii code of an uppercase letter, else returns c.
+ */
+int		ft_tolower(int c);
+
+/**
+ * Converts the initial portion of a string into the corresponding integer.
+ * Ignores spaces before digits and accepts one sign.
+ * Behaviour is undefined if the initial portion of the given string does not correspond to an integer.
+ * 
+ * @param[in] str The string to be converted
+ * @returns Returns the converted integer.
+ */
+int		ft_atoi(const char *str);
+
+/**
+ * Converts the initial portion of a string into the corresponding long integer.
+ * Ignores spaces before digits and accepts one sign.
+ * Behaviour is undefined if the initial portion of the given string does not correspond to a long integer.
+ * 
+ * @param[in] str The string to be converted
+ * @returns Returns the converted long integer.
+ */
+long	ft_atol(const char *str);
+
+/**
+ * Converts the given integer into its string representation.
+ * 
+ * @param[in] n The integer to be converted
+ * @returns Returns the converted integer on success, else return 0.
+ */
+char	*ft_itoa(int n);
+
+//--- STRING FUNCTIONS ---//
 
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
@@ -58,11 +216,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	**ft_split(char const *s, char c);
 
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-int		ft_atoi(const char *str);
-long	ft_atol(const char *str);
-char	*ft_itoa(int n);
+//--- PUT FUNCTIONS ---//
 
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
@@ -71,6 +225,8 @@ void	ft_putnbr_fd(int n, int fd);
 void	ft_putunbr_fd(unsigned int n, int fd);
 void	ft_puthex_fd(unsigned int n, int use_capitals, int fd);
 void	ft_putaddr_fd(void *ptr, int fd);
+
+//--- LINKED LIST FUNCTIONS ---//
 
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
@@ -82,6 +238,8 @@ void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
+//--- PRINTF FUNCTIONS ---//
+
 int		ft_printf(const char *str, ...);
 int		ft_i_putchar(char c);
 int		ft_i_putstr(char *s);
@@ -90,6 +248,8 @@ int		ft_i_putunbr(unsigned int n);
 int		ft_i_puthex(unsigned int n, int use_capitals);
 int		ft_i_putaddr(void *ptr);
 int		ft_add_check_error(int value, int temp);
+
+//--- GET_NEXT_LINE FUNCTIONS ---//
 
 char	*get_next_line(int fd);
 ssize_t	ft_i_strchr(const char *s, int c);
