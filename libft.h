d5e0cdfafee0d96c7a890bdc22560778840bfe97
Henri Patsi <hpatsi@student.hive.fi>
2024-05-10 09:13:04 +0300

    added ft_stristype.c

--- libft.h
+++ libft.h
@@ -215,6 +215,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	**ft_split(char const *s, char c);
+int		ft_stristype(char *s, int (*f)(int));
 
 //--- PUT FUNCTIONS ---//
 
